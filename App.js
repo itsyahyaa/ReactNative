56fa66348ee641287b6959c8d8aba4315e041968
Yahyamohamed101 <yahyamohamed0001@gmail.com>
2021-07-31 17:28:12 +0300

    Touchables And Images

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome Home Abdimalik</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native</Text>
+      <TouchableNativeFeedback onPress={() => console.log("Pressed")}>
+        {/* <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        /> */}
+        <View
+          style={{ width: 200, height: 70, backgroundColor: "dodgerblue" }}
+        ></View>
+      </TouchableNativeFeedback>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
   },
 });
